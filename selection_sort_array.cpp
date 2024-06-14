50fcd8fb1b073505f23d7ba57de2b10eddaf4f5b
NIKHIL SINGH RAJPOOT <nikhil000021@gmail.com>
2024-06-14 22:15:24 +0530

    Add files via upload

---
+++ selection_sort_array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of an array:";
+    cin>>n;
+    int arr[n];
+    cout<<"enters elements of an array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
